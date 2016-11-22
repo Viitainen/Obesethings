7c94b3a5882e052fb0faa7c7e2bb6cfd89bfdb96
Arto Söderström <arto_sode@hotmail.com>
2016-11-22 19:49:28 +0200

    Add things action controller (edit, show, delete) to player show page

--- resources/views/player/show.blade.php
+++ resources/views/player/show.blade.php
@@ -30,6 +30,18 @@
                 <div class="col-md-6">
                     <div class="thing">
                         <h4>{{ $thing->title }}</h4>
+                        <span class="thing__actions">
+                            <a href="{{ action('ThingController@show', $thing->id) }}" title="Go to thing page."><i class="icon icon--green fa fa-eye"></i></a>
+                            @if(!Auth::guest())
+                            <a href="{{ action('ThingController@edit', $thing->id) }}" title="Edit thing."><i class="icon icon--green fa fa-edit"></i></a>
+                                {!! Form::open(['method' => 'DELETE',
+                                    'action' => ['ThingController@destroy', $thing->id],
+                                    'class' => 'form inline-block'])
+                                    !!}
+                                    <button title="Delete thing." class="btn-unstyled" type="submit" name="button" onClick = "return confirm('Are you sure you want to delete this item?');"><i class="icon icon--green fa fa-trash-o"></i></button>
+                                {!! Form::close() !!}
+                            @endif
+                        </span>
                         <div class="video-container">
                             <iframe width="100%"  height="100%" src="{{ $thing->url }}" frameborder="0" allowfullscreen></iframe>
                         </div>
